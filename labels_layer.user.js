4725eb7f081c6008e570e56f7bd1168938c116ed
le-jeu <64744459+le-jeu@users.noreply.github.com>
2021-05-11 07:41:55 +0000

    Deploying to dist from @ le-jeu/iitc-plugins@44bb8402007fd48edcc10e0f631ba2f736e015a3 🚀

--- labels_layer.user.js
+++ labels_layer.user.js
@@ -19,7 +19,7 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
 plugin_info.buildName = 'lejeu';
-plugin_info.dateTimeVersion = '2021-05-06-155434';
+plugin_info.dateTimeVersion = '2021-05-11-074155';
 plugin_info.pluginId = 'labels_layer';
 //END PLUGIN AUTHORS NOTE
 
